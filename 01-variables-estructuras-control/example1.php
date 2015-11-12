0a94ab99d88f512737e1c6280a998d641c3bf447
Daniel Lerner <lernerd@gmail.com>
2015-11-12 16:28:40 -0300

    Ejemplos de practica 1

---
+++ 01-variables-estructuras-control/example1.php
@@ -0,0 +1,11 @@
+<?php
+
+$thisDay = "Martes";
+$weather = "lluvioso";
+
+echo "Hoy es " . $thisDay . ", el clima está " . $weather . "\n";
+
+$aNumber = 10;
+$anotherNumber = 3;
+echo "Tengo dos números: " . $aNumber . " y " . $anotherNumber . "\n";
+echo "Sumados dan un total de " . ($aNumber + $anotherNumber) . "\n";
---
+++ 01-variables-estructuras-control/example2.php
@@ -0,0 +1,12 @@
+<?php
+
+$thisDay = "Martes";
+$weather = "lluvioso";
+
+echo "Hoy es $thisDay, el clima está $weather.\n";
+
+$numbers = [1,2,3];
+echo "Contando...\n";
+foreach($numbers as $number) {
+  echo $number."\n";
+}
