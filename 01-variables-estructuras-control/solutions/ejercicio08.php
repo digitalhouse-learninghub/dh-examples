956b81b6a3318bc2e6d8a6e9e03a19cb7414c4c1
Daniel Lerner <lernerd@gmail.com>
2015-11-24 10:51:13 -0300

    Solucion a ejercicio 1.8 #2

---
+++ 01-variables-estructuras-control/solutions/ejercicio08.php
@@ -0,0 +1,32 @@
+<?php
+
+$info = [];
+$info['name'] = "Daniel";
+$info['lastName'] = "Lerner";
+$info['email'] = "daniel@digitalhouse.com";
+
+$info['country'] = "Argentina";
+$info['birth'] = 1983;
+$info['dni'] = "30083210";
+
+$currentYear = 2015;
+$info['age'] = $currentYear - $info['birth'];
+
+$moreInfo = $info;
+$moreInfo['name'] = "Sebastian";
+$moreInfo['lastName'] = "Gagin";
+$moreInfo['email'] = "sebas@digitalhouse.com";
+$moreInfo['country'] = "Uruguay";
+$moreInfo['birth'] = 1985;
+$moreInfo['dni'] = "31233510";
+$moreInfo['age'] = 30;
+
+echo "Primer arreglo...\n";
+foreach ($info as $key => $value) {
+  if($key != 'age') echo "$key: $value\n";
+}
+
+echo "Segundo arreglo...\n";
+foreach ($moreInfo as $key => $value) {
+  if($key != 'age') echo "$key: $value\n";
+}
