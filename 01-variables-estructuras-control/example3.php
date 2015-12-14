af61351872c6aee6b6411785d7c288197a2fb1a2
Daniel Lerner <lernerd@gmail.com>
2015-12-14 14:23:26 -0300

    Agrego ejemplo 3 para tomar input del usuario por CLI

---
+++ 01-variables-estructuras-control/example3.php
@@ -0,0 +1,5 @@
+<?php
+
+$userName = readline("¡Hola! ¿Cuál es tu nombre? ");
+// Recordatorio: dentro de una cadena con doble comillas, la \n imprime un (enter) o un salto de linea
+echo "Bienvenid@ $userName\n";
