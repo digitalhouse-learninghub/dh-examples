f0feff8679385f762effa2a4b4aebdbb1299548b
Daniel Lerner <lernerd@gmail.com>
2015-12-15 15:09:11 -0300

    Ejercicio 1 de embed

---
+++ 02-html-embed-requests/README.md
@@ -0,0 +1,15 @@
+# 02 - HTML embed y requests de http
+
+Al finalizar cada práctica, el archivo php debe ejecutarse por línea de comando de la siguiente forma:
+```shell
+php base.php
+```
+
+### Ejercicios
+1. Editar el archivo `base.php` y reemplazar todos los textos (incluído el título del documento) por variables de php.
+
+### Actividad integradora
+#### Contexto
+Estamos armando una aplicación para tomar notas. Del tipo que usarías si estás en una reunión o en una clase y querés escribir lo que vas escuchando para poder repasarlo luego.
+
+#### Ejercicios
---
+++ 02-html-embed-requests/base.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Título de la página</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <!-- Custom styles for this template -->
+    <link href="css/template.css" rel="stylesheet">
+  </head>
+  <body>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Nombre del proyecto</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Inicio</a></li>
+            <li><a href="#about">Info</a></li>
+            <li><a href="#contact">Contacto</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Template básico de Bootstrap</h1>
+        <p class="lead">Este es el comienzo básico de un proyecto.<br> Solo hay texto y una estructura de HTML.</p>
+      </div>
+
+    </div><!-- /.container -->
+  </body>
+</html>
